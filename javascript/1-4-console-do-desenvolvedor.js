45b252bfd47c937317b72ac6e23ff3f415238f87
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-04-22 13:52:36 -0300

    ADD: console-do-desenvolvedor CREATE: javascript-Folder

---
+++ javascript/1-4-console-do-desenvolvedor.js
@@ -0,0 +1,59 @@
+/* 
+    Console do desenvolvedor
+
+        Código é propenso a erros. Muito provavelmente você cometerá erros... Oh, do que estou falando? Você certamente cometerá erros, pelo menos se for humeno, não um robô.
+
+        Mas no navegador, os usuários não veem errors por padrão. Portanto, se algo der errado no script, não veremos o que está errado e não poderemos concertá-lo.
+
+        Para ver erros e obter muitas outras informações úteis sobre o script, as "ferramentas do desenvolvedor" foram incorporadas aos navegadores.
+
+        A maioria dos desenvolvedores se inclina para o Chrome ou Firefox para desenvolvimento porque esses navegadores têm as melhores ferramentas para desenvolvedores. Outros navegadores também fornecem ferramentas para desenvolvedores, às vezes com recursos especiais, mas geralmente estão "recuperando" o Chrome ou Firefox. Portanto a maioria dos desenvolvedores tem um navegador "favorito" e muda para outro se um problema for específico do navegador.
+
+        As ferramentas do desenvolvedor são potentes; eles tem muitos recursos. Para começar, aprenderemos como abri-los, verificar erros e executar comandos JavaScript.
+
+            Google Chrome
+
+                Abra sua página
+
+                Há um erro no código JavaScript nele. Ele está oculto aos olhos de um visitante regular, então vamos abrir as ferramentas do desenvolvedor para vê-lo.
+
+                Pressione "F-12", ou se estiver no Mac "Cmd+Opt+J".
+
+                A aparência exata das ferramentas do desenvolvedor depende da sua versão do chrome. Ele muda de tempos em tempos, mas deve ser semelhante.
+
+                    Aqui podemos ver a mensagem de erro em vermelho
+
+                    À direita, há um link clicável para a fonte do erro com o número da linha onde ocorreu o erro.
+
+                Abaixo da mensagem de erro, há um sinal ">" em azul. Ele marca um "linha de comando" onde podemos digitar comandos JavaScript. Pressione "Enter" para executá-los.
+
+                Agora podemos ver erros, e isso é suficiente para começar. Voltaremos as ferramentas do desenvolvedor mais tarde e abordaremos a depuração com mais profundidade no capítulo Deouração no navegador.
+
+                DICA
+
+                    Entrada multilinha
+
+                    Normalmente, quando colocamos uma linha de código no console e pressionamos "Enter", ele é executado.
+
+                    Para inserir várias linhas, presione "Shift+Enter". Dessa forma, é possível inserir longos fragmentos de código JavaScript.
+
+            Firefox, Edge e outros
+
+                A maioria dos outro navegadores us "F12" para abrir a ferramenta do desenvolvedor.
+
+                A aparência deles é bastante semelhante. Depois de saber usar uma dessas ferramentas, você pode facilmente mudar para outra.
+
+            Sfári
+
+                O Safári(navegador do Mac, não compativel com Windows/Linux) é um pouco especial aqui. Presisamos habilitar o "menu Desenvolvedor" primeiro.
+
+                Abra Preferências e vá para o painel "Avançado". Há uma caixa de seleção na parte inferior.
+
+                Agora pode alternar no console "Cmd+Opt+C". Além disso, observe que o novo item de "Desenvolvedor" no menu superior apareceu. Tem muito comando e opções.
+
+        Resumo
+
+            As ferramentas do desenvolvedor nos permitem ver erros, executar comandos, examinar varáveis e muito mais.
+
+            Eles podem ser abertos na maioria dos navegadores no Windows "F-12". Chrome no Mac "Cmd+Opt+J", Safári "Cmd+Opt+C"(precisa habilitar primeiro).
+*/
